44f05a3267ac0c1f4c73483fc08d4ce032c6b7b8
biyonik <ahmet.altun60@gmail.com>
2024-12-01 22:08:38 +0300

    Initialised back-end application

---
+++ usermanagementportal/src/main/java/com/ahmetaltun/usermanagementportal/UsermanagementportalApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetaltun.usermanagementportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementportalApplication.class, args);
+	}
+
+}
